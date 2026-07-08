5c83d41640779d30a7fe7005496171bc33b84696
KongNhel <kabkong94@gmail.com>
2026-07-08 19:59:30 +0700

    update image upload

--- config/mailer.js
+++ config/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const createTransporter = () => {
     return nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        secure: false,
+        port: parseInt(process.env.EMAIL_PORT) || 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
